0ead2f9a683baffe78912561eb877cbb09fbfb13
Hrishabh Singh <rishab18011@gmail.com>
2024-10-24 16:36:45 +0530

    feat: Add DropZone component for file uploads
    This commit adds a new DropZone component to the application, enhancing the file upload experience with drag-and-drop functionality. The DropZone offers a user-friendly interface for handling file uploads visually.
    
    - Introduced DropZone component to wrap existing App content and detect file drops.
    - Created `UploadIcon` to indicate file drop actions within the DropZone.
    - Added `iconProps.ts` interface for defining icon property types, with future extensibility in mind.
    
    These updates enhance file upload functionality and improve overall user interaction.

---
+++ src/utils/handleMediaFiles.ts
@@ -0,0 +1,48 @@
+import type { Track } from "@/types/mediaTypes";
+import { addTrack } from "@/features/playlist/playlistSlice";
+import { Dispatch, UnknownAction } from "@reduxjs/toolkit/react";
+
+// Helper to determine media type (audio or video)
+const getMediaElementType = (fileType: string) =>
+  fileType.startsWith("video") ? "video" : "audio";
+
+// Handle file uploads and extract metadata
+export const handleValidMediaFiles = async (
+  files: File[],
+  dispatch: Dispatch<UnknownAction>,
+) => {
+  if (!files || files.length === 0) return; // Early return for empty files
+
+  files.forEach((file) => {
+    const fileUrl = URL.createObjectURL(file);
+    const mediaType = getMediaElementType(file.type);
+    const mediaElement = document.createElement(mediaType); // Create video or audio element
+
+    mediaElement.src = fileUrl;
+
+    // Event listener to extract metadata once it's available
+    mediaElement.addEventListener("loadedmetadata", () => {
+      const track: Track = {
+        id: fileUrl.substring(fileUrl.lastIndexOf("/") + 1),
+        name: file.name,
+        url: fileUrl,
+        mediaCategory: mediaType,
+        fileFormat: file.name.substring(file.name.lastIndexOf(".") + 1),
+        fileSizeInBytes: file.size,
+        durationInSeconds: mediaElement.duration,
+        dateAddedToPlaylist: new Date().toISOString(),
+        isCurrentlyPlaying: false,
+        isFavorite: false,
+        isLoopActive: false,
+        shouldBeSkipped: false,
+        hasBeenPlayedBefore: false,
+      };
+
+      // Dispatch track to playlist
+      dispatch(addTrack(track));
+    });
+
+    // Load media to trigger 'loadedmetadata' event
+    mediaElement.load();
+  });
+};
